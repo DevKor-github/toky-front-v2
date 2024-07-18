2f6a7c4a9b488ebbbfee0ae45e8686c1ff3ff6df
SeokWon <tjrdnjs1534@naver.com>
2024-07-18 21:15:54 +0900

    feat: rename lib to libs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import StyledComponentsRegistry from "@/lib/registry";
+import StyledComponentsRegistry from "@/libs/registry";
 
 export const metadata: Metadata = {
   title: "Create Next App",
