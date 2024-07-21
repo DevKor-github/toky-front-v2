0135f3f935ae3d38f3d8b12047c1fe95e27c9789
SeokWon <tjrdnjs1534@naver.com>
2024-07-21 18:40:24 +0900

    feat: add some icons

---
+++ libs/design-system/icons/Ticket.tsx
@@ -0,0 +1,88 @@
+export function Ticket() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="23" viewBox="0 0 22 23" fill="none">
+      <g filter="url(#filter0_i_262_4523)">
+        <path
+          d="M2.19971 7.87998C2.19971 6.96871 2.93844 6.22998 3.8497 6.22998L13.9075 6.22998C13.8766 6.30641 13.8595 6.38995 13.8595 6.47748V7.30247C13.8595 7.66697 14.155 7.96246 14.5195 7.96246V9.11744C14.155 9.11744 13.8595 9.41293 13.8595 9.77743V10.6024C13.8595 10.9669 14.155 11.2624 14.5195 11.2624V12.4174C14.155 12.4174 13.8595 12.7129 13.8595 13.0774V13.9024C13.8595 14.2669 14.155 14.5624 14.5195 14.5624V15.7173C14.155 15.7173 13.8595 16.0128 13.8595 16.3773V17.2023C13.8595 17.385 13.9338 17.5503 14.0537 17.6698H3.84971C2.93844 17.6698 2.19971 16.9311 2.19971 16.0198V14.0258C2.19971 13.8085 2.33254 13.6178 2.50909 13.4912C2.9545 13.1719 3.24469 12.6498 3.24469 12.0599C3.24469 11.47 2.9545 10.9479 2.50909 10.6286C2.33254 10.502 2.19971 10.3113 2.19971 10.094V7.87998Z"
+          fill="url(#paint0_linear_262_4523)"
+        />
+      </g>
+      <g filter="url(#filter1_i_262_4523)">
+        <path
+          d="M14.5195 9.11768V7.9627C14.884 7.9627 15.1795 7.66721 15.1795 7.30271V6.47772C15.1795 6.3902 15.1625 6.30665 15.1315 6.23022H18.1495C19.0607 6.23022 19.7995 6.96896 19.7995 7.88023V10.2184C19.7995 10.4147 19.6902 10.5912 19.5406 10.7183C19.1606 11.0411 18.9195 11.5225 18.9195 12.0601C18.9195 12.5978 19.1606 13.0792 19.5406 13.402C19.6902 13.5291 19.7995 13.7056 19.7995 13.9019V16.0201C19.7995 16.9313 19.0607 17.6701 18.1495 17.6701H14.9854C15.1053 17.5506 15.1795 17.3852 15.1795 17.2026V16.3776C15.1795 16.0131 14.884 15.7176 14.5195 15.7176V14.5626C14.884 14.5626 15.1795 14.2671 15.1795 13.9026V13.0776C15.1795 12.7131 14.884 12.4176 14.5195 12.4176V11.2627C14.884 11.2627 15.1795 10.9672 15.1795 10.6027V9.77767C15.1795 9.41317 14.884 9.11768 14.5195 9.11768Z"
+          fill="url(#paint1_linear_262_4523)"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_i_262_4523"
+          x="2.19971"
+          y="6.11998"
+          width="12.3198"
+          height="11.5499"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="-0.55" />
+          <feGaussianBlur stdDeviation="0.055" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0" />
+          <feBlend mode="normal" in2="shape" result="effect1_innerShadow_262_4523" />
+        </filter>
+        <filter
+          id="filter1_i_262_4523"
+          x="14.5195"
+          y="6.12022"
+          width="5.27979"
+          height="11.5499"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="-0.55" />
+          <feGaussianBlur stdDeviation="0.055" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0" />
+          <feBlend mode="normal" in2="shape" result="effect1_innerShadow_262_4523" />
+        </filter>
+        <linearGradient
+          id="paint0_linear_262_4523"
+          x1="7.47963"
+          y1="5.34999"
+          x2="7.47963"
+          y2="22.0698"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="#FFE771" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_262_4523"
+          x1="17.1595"
+          y1="6.23022"
+          x2="17.1595"
+          y2="17.6701"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFEC41" />
+          <stop offset="1" stopColor="#FFD338" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
