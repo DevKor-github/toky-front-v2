ab07b49f48a46796c4eb22508b611248109c9f38
SeokWon <tjrdnjs1534@naver.com>
2024-07-21 18:39:50 +0900

    feat: add typography and color

--- libs/design-system/styles/colors.ts
+++ libs/design-system/styles/colors.ts
@@ -1,6 +1,9 @@
 export const colors = {
   primary: '#000000',
   topBar: '#121212',
+  gradientButtonBlue: 'linear-gradient(180deg, #4C0EB0 0%, rgba(76, 14, 176, 0.60) 100%)',
+  gradientButtonGray:
+    'linear-gradient(180deg, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.05) 100%), #121212  ',
 } as const;
 
 export type Colors = keyof typeof colors;
