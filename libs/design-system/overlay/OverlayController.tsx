fe27a67e573f8a923276ac8374a23ea99a020e44
SeokWon <tjrdnjs1534@naver.com>
2024-07-21 18:16:23 +0900

    feat: overlay 추가 toss slash 라이브러리 코드

---
+++ libs/design-system/overlay/OverlayController.tsx
@@ -0,0 +1,38 @@
+import { forwardRef, Ref, useCallback, useEffect, useImperativeHandle, useState } from 'react';
+
+import { CreateOverlayElement } from './types';
+
+interface Props {
+  overlayElement: CreateOverlayElement;
+  onExit: () => void;
+}
+
+export interface OverlayControlRef {
+  close: () => void;
+}
+
+export const OverlayController = forwardRef(function OverlayController(
+  { overlayElement: OverlayElement, onExit }: Props,
+  ref: Ref<OverlayControlRef>,
+) {
+  const [isOpenOverlay, setIsOpenOverlay] = useState(false);
+
+  const handleOverlayClose = useCallback(() => setIsOpenOverlay(false), []);
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return { close: handleOverlayClose };
+    },
+    [handleOverlayClose],
+  );
+
+  useEffect(() => {
+    // NOTE: requestAnimationFrame이 없으면 가끔 Open 애니메이션이 실행되지 않는다.
+    requestAnimationFrame(() => {
+      setIsOpenOverlay(true);
+    });
+  }, []);
+
+  return <OverlayElement isOpen={isOpenOverlay} close={handleOverlayClose} exit={onExit} />;
+});
