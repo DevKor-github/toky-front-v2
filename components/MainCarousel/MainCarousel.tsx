11f241d8ed834ef02429b4e959065bdcf907ce3f
SeokWon <tjrdnjs1534@naver.com>
2024-07-24 16:12:37 +0900

    style: 컴포넌트 아래로 이동

--- components/MainCarousel/MainCarousel.tsx
+++ components/MainCarousel/MainCarousel.tsx
@@ -9,57 +9,6 @@ import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-const PaginationContainer = css`
-  width: 37px;
-  height: 21px;
-  position: absolute;
-  left: unset !important;
-  right: 20px;
-  bottom: 12px;
-  display: flex;
-  padding: 3px 8px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 99px;
-  background: rgba(0, 0, 0, 0.30);
-
-  .swiper-pagination-current {
-  color: var(--white-high-emphasis-87, rgba(255, 255, 255, 0.87));
-  font-family: "Spoqa Han Sans Neo";
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  letter-spacing: 0.72px;
-  }
-
-  .swiper-pagination-slash {
-  color: var(--white-high-emphasis-87, rgba(255, 255, 255, 0.87));
-  font-family: "Spoqa Han Sans Neo";
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  letter-spacing: 0.72px;
-  }
-
-  .swiper-pagination-total {
-  color: var(--white-medium-emphasis-60, rgba(255, 255, 255, 0.60));
-  font-family: "Spoqa Han Sans Neo";
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  letter-spacing: 0.72px;
-  `;
-// TODO: 테마 색상 적용
-
-const Wrapper = styled.div`
-  .swiper-pagination {
-    ${PaginationContainer}
-  }
-`;
-
 export function MainCarousel() {
   const bannersList = [banner1, banner1, banner1]; // TODO: 서버에서 이미지 파일 가져와서 생성
   return (
@@ -85,3 +34,55 @@ export function MainCarousel() {
     </Wrapper>
   );
 }
+
+const PaginationContainer = css`
+  width: 37px;
+  height: 21px;
+  position: absolute;
+  left: unset !important;
+  right: 20px;
+  bottom: 12px;
+  display: flex;
+  padding: 3px 8px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 99px;
+  background: rgba(0, 0, 0, 0.3);
+
+  .swiper-pagination-current {
+    color: var(--white-high-emphasis-87, rgba(255, 255, 255, 0.87));
+    font-family: 'Spoqa Han Sans Neo';
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: 0.72px;
+  }
+
+  .swiper-pagination-slash {
+    color: var(--white-high-emphasis-87, rgba(255, 255, 255, 0.87));
+    font-family: 'Spoqa Han Sans Neo';
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: 0.72px;
+  }
+
+  .swiper-pagination-total {
+    color: var(--white-medium-emphasis-60, rgba(255, 255, 255, 0.6));
+    font-family: 'Spoqa Han Sans Neo';
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: 0.72px;
+  }
+`;
+// TODO: 테마 색상 적용
+
+const Wrapper = styled.div`
+  .swiper-pagination {
+    ${PaginationContainer}
+  }
+`;
