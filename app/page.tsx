6774f648bf88fcd0a7df3dbaf9552ed65880564d
SeokWon <tjrdnjs1534@naver.com>
2024-07-24 16:12:06 +0900

    feat: add icon button

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 import MainTopBar from '@/components/MainTopBar';
 import MainCarousel from '@/components/MainCarousel';
 import styled from 'styled-components';
+import { Flex } from '@/libs/design-system/flex';
+import IconButton from '@/components/IconButton';
+import { ICON_INFO_LIST } from './constants';
 
 export default function Home() {
   return (
@@ -10,6 +13,11 @@ export default function Home() {
       <MainTopBar />
       <Wrapper>
         <MainCarousel />
+        <Flex $justify="space-between" style={{ padding: '32px' }}>
+          {ICON_INFO_LIST.map((iconInfo) => (
+            <IconButton key={`${iconInfo.href}-${iconInfo.icon}`} {...iconInfo} />
+          ))}
+        </Flex>
       </Wrapper>
     </div>
   );
@@ -17,4 +25,5 @@ export default function Home() {
 
 const Wrapper = styled.div`
   padding-top: 66px;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
