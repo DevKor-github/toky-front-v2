5822bd499527ad32452c0d7302d179ea497a0a16
SeokWon <tjrdnjs1534@naver.com>
2024-07-25 13:05:21 +0900

    chore: ssh 및 local https 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/bets',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'web-share=(self https://192.168.1.35:3001))',
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
 
     return config;
