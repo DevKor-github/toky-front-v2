56d9e501dab6dd9eac856f67af16c7a619056c82
SeokWon <tjrdnjs1534@naver.com>
2024-07-26 10:46:28 +0900

    feat: 기본 훅들 추가

---
+++ libs/hooks/useRefEffect.ts
@@ -0,0 +1,35 @@
+import { noop } from '../utils/noop';
+import { DependencyList, useCallback, useRef } from 'react';
+import { usePreservedCallback } from './usePreservedCallback';
+
+export type EffectRef<E extends HTMLElement = HTMLElement> = (element: E | null) => void;
+
+export type CleanupCallback = () => void;
+export type RefCallback<E extends HTMLElement = HTMLElement> = (element: E) => CleanupCallback | void;
+
+export function useRefEffect<E extends HTMLElement = HTMLElement>(
+  callback: RefCallback<E>,
+  deps: DependencyList,
+): EffectRef<E> {
+  const preservedCallback = usePreservedCallback(callback);
+  const disposeRef = useRef<CleanupCallback>(noop);
+
+  const effect = useCallback(
+    (element: E | null) => {
+      disposeRef.current();
+      disposeRef.current = noop;
+
+      if (element != null) {
+        const cleanup = callback(element);
+
+        if (typeof cleanup === 'function') {
+          disposeRef.current = cleanup;
+        }
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [preservedCallback, ...deps],
+  );
+
+  return effect;
+}
