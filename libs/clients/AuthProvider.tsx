d1adbb6092e794dd6e841a90399d391ee8d1717e
halionaz <samhsj@naver.com>
2024-08-21 21:05:42 +0900

    mod: getCookieValue 선언 방식

--- libs/clients/AuthProvider.tsx
+++ libs/clients/AuthProvider.tsx
@@ -9,27 +9,25 @@ export function AuthProvider() {
   const { accessToken, refreshToken, clearTokens, setTokens } = useAuthStore();
 
   const setTokenFromCookie = useCallback(() => {
-    const accessTokenFromCookie = getCookieValue('access-token'),
-      refreshTokenFromCookie = getCookieValue('refresh-token');
+    const accessTokenFromCookie = getCookieValue('access-token');
+    const refreshTokenFromCookie = getCookieValue('refresh-token');
 
     console.log(`access: ${accessTokenFromCookie}`);
     console.log(`refresh: ${refreshTokenFromCookie}`);
 
-    if (!accessTokenFromCookie || !refreshTokenFromCookie) {
+    if (accessTokenFromCookie === null || refreshTokenFromCookie === null) {
       clearTokens();
-      return false;
+      return;
     }
 
     document.cookie = 'access-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     document.cookie = 'refresh-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 
     setTokens(accessTokenFromCookie, refreshTokenFromCookie);
-
-    return true;
   }, [clearTokens, setTokens]);
 
   const checkAuth = useCallback(async () => {
-    if (refreshToken) {
+    if (refreshToken !== null) {
       await refresh(process.env.NEXT_PUBLIC_API_URL ?? '');
     } else {
       setTokenFromCookie();
