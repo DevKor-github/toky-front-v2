9acb2bc9d49b1d125509784fb6bab4ee75231714
halionaz <samhsj@naver.com>
2024-08-21 23:32:37 +0900

    fix: axios client AuthProvider에서 config 설정

--- libs/client/AuthProvider.tsx
+++ libs/client/AuthProvider.tsx
@@ -1,20 +1,72 @@
 'use client';
 
+import client from '@/libs/client/client';
 import { useAuthStore } from '@/libs/store/useAuthStore';
 import { getCookie } from '@/libs/utils/getCookie';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 import { useEffect } from 'react';
 
 export function AuthProvider() {
   const { accessToken, refreshToken, setTokens, clearTokens } = useAuthStore((state) => state);
 
+  const errorHandler = async (error: any) => {
+    const { config, response } = error;
+    const originalRequest = config;
+
+    if (response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const { data } = await axios.post<{ accessToken: string; refreshToken: string }>(
+          `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+            withCredentials: true,
+          },
+        );
+
+        setTokens(data.accessToken, data.refreshToken);
+
+        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+        return client(originalRequest);
+      } catch (err) {
+        clearTokens();
+        // 필요시 로그아웃 처리
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  };
+
+  const requestHandler = (config: InternalAxiosRequestConfig) => {
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  };
+
+  const requestInterceptor = client.interceptors.request.use(requestHandler, (error) => Promise.reject(error));
+
+  const responseInterceptor = client.interceptors.response.use((response) => response, errorHandler);
+
   useEffect(() => {
-    console.log('zustand 저장 값');
+    return () => {
+      client.interceptors.request.eject(requestInterceptor);
+      client.interceptors.response.eject(responseInterceptor);
+    };
+  }, [responseInterceptor, requestInterceptor]);
+
+  useEffect(() => {
+    console.log('token from zustand');
     console.log(accessToken, refreshToken);
     if (accessToken === null) {
       if (refreshToken === null) {
         const accessTokenFromCookie = getCookie('access-token');
         const refreshTokenFromCookie = getCookie('refresh-token');
-        console.log('쿠키 저장 값');
+        console.log('token from cookie');
         console.log(accessTokenFromCookie, refreshTokenFromCookie);
 
         if (accessTokenFromCookie === null || refreshTokenFromCookie === null) {
--- libs/client/client.ts
+++ libs/client/client.ts
@@ -1,43 +1,8 @@
-import { useRefresh } from '@/libs/client/refresh';
-import { useAuthStore } from '@/libs/store/useAuthStore';
 import axios from 'axios';
 
 const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL ?? '',
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  withCredentials: false,
 });
-
-client.interceptors.request.use(
-  (config) => {
-    const { accessToken } = useAuthStore();
-    if (accessToken && config.headers) {
-      config.headers['Authorization'] = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
-
-client.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const { config, response } = error;
-    const originalRequest = config;
-    if (response?.status === 401) {
-      const { refresh } = useRefresh();
-      const newAccessToken = await refresh();
-
-      if (newAccessToken !== null) {
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-        return client.request(originalRequest);
-      }
-    }
-
-    return Promise.reject(error);
-  },
-);
 
 export default client;
