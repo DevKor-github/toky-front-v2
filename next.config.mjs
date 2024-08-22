9fdc334596654250c1e99c87bdb99a28e0f89ebc
SeokWon <tjrdnjs1534@naver.com>
2024-08-22 21:46:11 +0900

    fix: origin header

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'dev.toky.devkor.club' },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
