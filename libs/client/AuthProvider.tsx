6a5153b81415093347fae472b7aff4a3349e0122
SeokWon <tjrdnjs1534@naver.com>
2024-08-26 22:09:40 +0900

    feat: refreshtoken 널이면 refresh 시도 x

--- libs/client/AuthProvider.tsx
+++ libs/client/AuthProvider.tsx
@@ -39,7 +39,7 @@ export function AuthProvider() {
     const originalRequest = config;
 
     if (response?.status === 401 && config.url !== REFRESH_URL && !config.sent) {
-      if (!refreshToken) return Promise.reject(error);
+      if (refreshToken == null) return Promise.reject(error);
       config.sent = true;
       const newAccessToken = await refresh();
 
