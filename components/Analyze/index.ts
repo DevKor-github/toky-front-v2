014c1e822866facee842511aec4f0f49bda8a36c
junelibre <tx0203@gmail.com>
2024-08-27 15:16:18 +0900

    feat(analyze/details): 더알아보기 페이지 제작

--- components/Analyze/index.ts
+++ components/Analyze/index.ts
@@ -1,3 +1,4 @@
 export { AnalyzeTotal } from './AnalyzeTotal';
 export { MatchInfo } from './MatchInfo';
 export { AnalyzeMatch } from './AnalyzeMatch';
+export { PlayerCardContainer } from './PlayerCardContainer';
--- libs/design-system/styles/space.ts
+++ libs/design-system/styles/space.ts
@@ -1,5 +1,6 @@
 export const space = {
   mainTopBarHeight: 49,
   navigationBarHeight: 46,
+  sportsSelectionBarHeight: 44,
   predictionBannerHeight: 170,
 } as const;
