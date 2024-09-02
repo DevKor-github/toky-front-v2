477f41952b32dfe67a8b12ab62f4a8183dbc6326
junelibre <tx0203@gmail.com>
2024-09-02 23:00:45 +0900

    feat(ticket-history): 디자인, 기능 구현

--- app/ticket-history/page.tsx
+++ app/ticket-history/page.tsx
@@ -3,9 +3,32 @@
 import styled from 'styled-components';
 import Header from '@/components/Header';
 import { useGetTicketHistoryList } from '@/libs/apis/tickets';
+import { Flex } from '@/libs/design-system/flex';
 
 export default function TicketHistory() {
   const { data: ticketHistoryList } = useGetTicketHistoryList() || [];
+
+  const dataFormat = (dateString: string) => {
+    const regex = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.\d{3}Z$/;
+    const match = dateString.match(regex);
+
+    if (match) {
+      const year = match[1].slice(-2); // 연도의 마지막 두 자리
+      const month = match[2];
+      const day = match[3];
+      const hours = match[4];
+      const minutes = match[5];
+
+      return `${year}.${month}.${day} ${hours}:${minutes}`;
+    }
+  };
+
+  const usedTicketFormat = (usedTicket: number) => {
+    if (usedTicket < 0) {
+      return `${usedTicket}`;
+    }
+    return `+${usedTicket}`;
+  };
 
   return (
     <>
@@ -14,10 +37,18 @@ export default function TicketHistory() {
         <TicketHistoryList>
           {ticketHistoryList?.map((ticketHistory) => (
             <TicketHistoryItem key={ticketHistory.id}>
-              <TicketHistoryItemTitle>{ticketHistory.detail}</TicketHistoryItemTitle>
-              <TicketHistoryItemDate>{ticketHistory.createdAt}</TicketHistoryItemDate>
-              <TicketHistoryItemTicket>{ticketHistory.usedTicket}개 사용</TicketHistoryItemTicket>
-              <TicketHistoryItemTicket>{ticketHistory.remainingTicket}개 남음</TicketHistoryItemTicket>
+              <Flex $justify="space-between">
+                <Flex $direction="column">
+                  <TicketHistoryItemTitle>{ticketHistory.detail}</TicketHistoryItemTitle>
+                  <TicketHistoryItemDate>{dataFormat(ticketHistory.createdAt)}</TicketHistoryItemDate>
+                </Flex>
+                <Flex $direction="column" $align="flex-end">
+                  <TicketHistoryItemUsedTicket>
+                    {usedTicketFormat(ticketHistory.usedTicket)}장
+                  </TicketHistoryItemUsedTicket>
+                  <TicketHistoryItemRemainingTicket>{ticketHistory.remainingTicket}장</TicketHistoryItemRemainingTicket>
+                </Flex>
+              </Flex>
             </TicketHistoryItem>
           ))}
         </TicketHistoryList>
@@ -36,8 +67,8 @@ const TicketHistoryList = styled.div`
 `;
 
 const TicketHistoryItem = styled.div`
-  padding: 16px 0;
-  border-bottom: 1px solid var(--gray_2, #e5e5e5);
+  padding: 25px 0px 15px 0px;
+  border-bottom: 0.5px solid var(--_15, rgba(255, 255, 255, 0.15));
 `;
 
 const TicketHistoryItemTitle = styled.div`
@@ -53,7 +84,22 @@ const TicketHistoryItemDate = styled.div`
   margin-bottom: 8px;
 `;
 
-const TicketHistoryItemTicket = styled.div`
-  font-size: 14px;
-  color: var(--gray_6, #757575);
+const TicketHistoryItemUsedTicket = styled.div`
+  color: var(--white_0, #fff);
+  text-align: right;
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+const TicketHistoryItemRemainingTicket = styled.div`
+  color: var(--_38, rgba(255, 255, 255, 0.38));
+  text-align: right;
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `;
