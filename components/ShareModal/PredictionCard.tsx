68997e45d506d4dc2c4f1a45c4cb8a7766006751
SeokWon <tjrdnjs1534@naver.com>
2024-09-07 19:39:26 +0900

    feat: 다시 ref로

--- components/ShareModal/PredictionCard.tsx
+++ components/ShareModal/PredictionCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Flex } from '@/libs/design-system/flex';
 import { Icon } from '@/libs/design-system/icons';
-import { ForwardedRef, forwardRef, useRef } from 'react';
+import { ForwardedRef, forwardRef, use, useRef } from 'react';
 import styled, { css } from 'styled-components';
 import { DRAW_IMAGE_LIST, KOREA_WIN_IMAGE_LIST, YONSEI_WIN_IMAGE_LIST } from './constants';
 
@@ -24,7 +24,7 @@ export function PredictionCardFC(
         ? YONSEI_WIN_IMAGE_LIST
         : DRAW_IMAGE_LIST;
 
-  const predictionImgSrc = charaterSrcList[Math.floor(Math.random() * charaterSrcList.length)];
+  const predictionImgSrc = useRef(charaterSrcList[Math.floor(Math.random() * charaterSrcList.length)]).current;
 
   return (
     <ShareCardWrapper ref={ref}>
